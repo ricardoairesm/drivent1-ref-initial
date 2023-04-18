100411454b98f0c5018ed2150e99f9a3fcb1169c
ricardoairesm <ricardoaamb@usp.br>
2023-04-18 09:35:03 -0300

    fix: using the bode validation midlleware

--- src/protocols.ts
+++ src/protocols.ts
@@ -38,3 +38,21 @@ export type Payment = {
     cvv: number;
   };
 };
+
+export type Ticket = {
+  id: number;
+  status: string; //RESERVED | PAID
+  ticketTypeId: number;
+  enrollmentId: number;
+  TicketType: {
+    id: number;
+    name: string;
+    price: number;
+    isRemote: boolean;
+    includesHotel: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+  };
+  createdAt: Date;
+  updatedAt: Date;
+};
